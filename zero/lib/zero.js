832cb07c67a50eb96ce0caac554f05ad4242040c
Enrico Marino <enrico.marino@email.com>
2012-03-21 11:23:13 +0100

    added zero lib

---
+++ zero/lib/zero.js
@@ -0,0 +1,23 @@
+
+/* !
+ * {{project.name}}
+ * {{project.description}}
+ * Copyright (c) 2012 {{author.name}} <{{author.email}}> ({{author.site}})
+ * MIT License
+ */
+
+ !(function (exports) {
+
+  /**
+   * Library namespace.
+   */
+
+  //var {{project.name}} = {{project.name}} = {};
+
+  /**
+   * Library version.
+   */
+
+  //{{project.name}}.version = '0.0.0';
+
+ }(this));
